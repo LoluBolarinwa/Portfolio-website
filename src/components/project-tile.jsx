1e264e1762841e7a7c3c789c4d6a668c98c1cd39
LoluBolarinwa <lolubolarinwa@gmail.com>
2022-07-10 23:39:33 +0100

    section 2 project update

--- src/components/project-tile.jsx
+++ src/components/project-tile.jsx
@@ -3,13 +3,17 @@ import React from "react";
 function Project(){
     return(
     <div className="project-tile">
+
+<img src="../CG797 Line & Dot Pattern Design.jpg"></img>
+
         <div className="project-content">
-            <link src="..components/CG797 Line & Dot Pattern Design.jpg"></link>
+        {/* <img src="images/picture.jpg" alt="Mountain"></img> */}
         </div>
         <h3 className="project-description">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione repudiandae 
         laboriosam eaque vitae quasi ipsam hic libero aliquam debitis. Itaque deleniti 
         </h3>
+        
     </div>
 
     )
