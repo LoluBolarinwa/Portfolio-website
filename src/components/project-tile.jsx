c8cbc7aa703462bf11eca3ff58ffa43a5b7c41fa
LoluBolarinwa <lolubolarinwa@gmail.com>
2022-07-10 20:44:16 +0100

    portfolio update

---
+++ src/components/project-tile.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Project(){
+    return(
+    <div className="project-tile">
+        <div className="project-content">
+            <link src="..components/CG797 Line & Dot Pattern Design.jpg"></link>
+        </div>
+        <h3 className="project-description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione repudiandae 
+        laboriosam eaque vitae quasi ipsam hic libero aliquam debitis. Itaque deleniti 
+        </h3>
+    </div>
+
+    )
+}
+
+export default Project;
--- src/components/section2.jsx
+++ src/components/section2.jsx
@@ -1,20 +1,16 @@
 import React from "react";
+import Project from "./project-tile";
+
 
 function Section2(){
     return(
 
         <div className="section-2">
-            <div className="project-tile">
-                <div className="project-content">
-                    
-                </div>
-                <h3 className="project-description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione repudiandae 
-                laboriosam eaque vitae quasi ipsam hic libero aliquam debitis. Itaque deleniti 
-                iure debitis ipsam vel quisquam necessitatibus sunt impedit quod.
-                </h3>
-            </div>
-        </div>    
+          <Project />
+          <Project />
+          <Project />
+
+        </div>   
         
     )
 }
