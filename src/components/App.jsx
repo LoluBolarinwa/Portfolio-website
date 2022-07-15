582d06d820c111bd12b898ca777d7c6bd9e3064d
LoluBolarinwa <lolubolarinwa@gmail.com>
2022-07-15 13:03:13 +0100

    combined section 1 and 2 into same jsx file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Section1 from "./section1";
-import Section2 from "./section2";
+// import Section2 from "./section2";
 import Section3 from "./section3";
 // import Navigation from "./navigation";
 
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       {/* <Navigation /> */}
       <Section1 />
-      <Section2 />
+      {/* <Section2 /> */}
       <Section3 />
       {/* <h1>Welcome to my portfolio Oduduwa</h1> */}
     </div>
