1bb4f6ad106bbf529d85653acf45c6be2cccb1f2
LoluBolarinwa <lolubolarinwa@gmail.com>
2022-07-23 01:37:50 +0100

    Figured why svg was throwing errors and obtained best svg icon code

--- src/components/icons.jsx
+++ src/components/icons.jsx
@@ -5,8 +5,28 @@ import React from "react";
 
 const Icon=()=>{
     return(
-        
-<div>me</div>
+<div>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="88px" height="88px">
+   <path fill="#E65100" d="M41,5H7l3,34l14,4l14-4L41,5L41,5z" />
+    <path fill="#FF6D00" d="M24 8L24 39.9 35.2 36.7 37.7 8z" />
+    <path fill="#FFF" d="M24,25v-4h8.6l-0.7,11.5L24,35.1v-4.2l4.1-1.4l0.3-4.5H24z M32.9,17l0.3-4H24v4H32.9z" />
+    <path fill="#EEE" d="M24,30.9v4.2l-7.9-2.6L15.7,27h4l0.2,2.5L24,30.9z M19.1,17H24v-4h-9.1l0.7,12H24v-4h-4.6L19.1,17z" />
+</svg>
+</div>
+/*
+ <div>
+ <svg width="96px" height="96px" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"> 
+     <title>file_type_solidity</title>
+     <path d="M20.477,2l-4.5,8h-9l4.5-8h9" fill="#c1c1c1" />
+     <path d="M11.52,30l4.5-8h9l-4.5,8h-9" fill="#c1c1c1" />
+     <path d="M15.975,10h9l-4.5-8h-9Z" fill="#c1c1c1" />
+     <path d="M16.022,22h-9l4.5,8h9Z" fill="#c1c1c1" />
+     <path d="M11.477,18l4.5-8-4.5-8-4.5,8Z" fill="#c1c1c1" />
+     <path d="M20.52,14l-4.5,8,4.5,8,4.5-8Z" fill="#c1c1c1" />
+ </svg>
+ </div>
+
+*/
 
     )
 }
