642459d17071569b8bd283b3ad0b40a9eb6dbde9
LoluBolarinwa <lolubolarinwa@gmail.com>
2022-07-25 20:30:56 +0100

    addded all svg icons and made them responsive

--- src/components/section1.jsx
+++ src/components/section1.jsx
@@ -89,22 +89,20 @@ function Section1() {
     </div>
 </div>
 
+<div className="about">
+
             <div className="about-me-div">
             <h2 className="about-me">About <span className="me">Me.</span>
                 <span className="me-underline"></span>
                 <p className="about-text">
                 Hi there, welcome to my Portfolio. [my] Journey into web3 
-                began when I cluelessly bought bitcoin in early winter
-                 of 2017 [and]forgot about it. I[later] remembered in January of 
-                 2021 and sold it at an appreciated value.
-                 This store of value was completely new to me and made me curious. 
+                began in early winter of 2017 when I cluelessly bought bitcoin 
+                 and forgot about it. I[later] remembered in January of 
+                 2021 and sold it.
+                This store of value was completely new to me and made me curious. 
                  So I researched about the type of internet architecture that kept 
-                 it running, and found [Blockchain] at its heart.
-                This led me down this web3 rabbit hole. 
-                about[led me into] 
-
-                about how the architecture which. So I researched it.
-                facilitates the which eventually led me down this web3 rabbit hole.
+                 it secure and running, and found Blockchain in the middle of it all.
+                This led me down this web3 rabbit hole.
 
                 </p>
             </h2>
@@ -113,7 +111,8 @@ function Section1() {
             <Project />
             <Project /> */}
 
-            </div>   
+            </div>
+</div>   
             {/* </div> */}
             </section>
         </div>
