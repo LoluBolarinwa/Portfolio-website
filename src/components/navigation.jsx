d6773b0f7ded031cd3f316a7383182bc3231799b
LoluBolarinwa <lolubolarinwa@gmail.com>
2022-07-26 15:53:54 +0100

    converting all sections into components to make it code base Neat

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,19 +1,18 @@
-// import React from "react";
+import React from "react";
 
 function Navigation(){
     return (
-        <div>
-            <nav>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Portfolio</a></li>
-                    <li><a href="#">Contacts</a></li>
+        <nav className="navbar">
+                <ul className="navbar-list">
+                    <li><a href="#Home">Home</a></li>
+                    <li><a href="#About">About</a></li>
+                    <li><a href="#Portfolio">Portfolio</a></li>
+                    <li><a href="#Contacts">Contacts</a></li>
                     {/* <li><a href="#"></a></li> */}
                 </ul>
             </nav>
-        </div>
+
     )
 }
 
-// export default Navigation;
+export default Navigation;
