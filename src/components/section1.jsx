998233b9ba9b11bcbbc1593ebaf9edf77ae634be
LoluBolarinwa <lolubolarinwa@gmail.com>
2022-07-30 23:39:34 +0100

    added space between icon in tool stack

--- src/components/section1.jsx
+++ src/components/section1.jsx
@@ -41,8 +41,7 @@ function Section1() {
                 </div>
             </div>
 
-        </section>    
-            
+        </section>                
 
         <section className="section-2">
    
