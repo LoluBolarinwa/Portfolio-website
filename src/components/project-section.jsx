e5b2e3933f36029a533f047d8b41a16468b21ac1
LoluBolarinwa <lolubolarinwa@gmail.com>
2022-08-01 21:24:05 +0100

    creating project section

--- src/components/project-section.jsx
+++ src/components/project-section.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Project(){
+function Projects(){
     return(
         <div id="project-div">
             <div className="project-parentDiv">
@@ -20,4 +20,4 @@ function Project(){
     )
 }
 
-export default Project;
+export default Projects;
