9db2444c2df0e202e45bd599377c1c75f6882df8
LoluBolarinwa <lolubolarinwa@gmail.com>
2022-08-01 21:36:08 +0100

    creating project section

--- src/components/section2.jsx
+++ src/components/section2.jsx
@@ -4,35 +4,6 @@ import React from "react";
 
 function Section2(){
     return(
-        // <section>
-        
-        //     <div className="section-2">
-        //     <div className="about-me-div">
-        //     <h2 className="about-me">About <span className="me">Me.</span>
-        //         <span className="me-underline"></span>
-        //         <p className="about-text">
-        //         Hi there, welcome to my Portfolio. [my] Journey into web3 
-        //         began when I cluelessly bought bitcoin in early winter
-        //          of 2017 [and]forgot about it. I[later] remembered in January of 
-        //          2021 and sold it at an appreciated value.
-        //          This store of value was completely new to me and made me curious. 
-        //          So I researched about the type of internet architecture that kept 
-        //          it running, and found [Blockchain] at its heart.
-        //         This led me down this web3 rabbit hole. 
-        //         about[led me into] 
-
-        //         about how the architecture which. So I researched it.
-        //         facilitates the which eventually led me down this web3 rabbit hole.
-
-        //         </p>
-        //     </h2>
-            
-            
-
-        //     </div>   
-        //     </div>
-
-        // </section>
         <div className="about">
 
             <div className="about-me-div">
