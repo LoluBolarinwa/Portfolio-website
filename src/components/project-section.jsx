625d12774a3882fdadf2b80a79da8bee37506d04
LoluBolarinwa <lolubolarinwa@gmail.com>
2022-08-03 00:21:35 +0100

    project section structure

--- src/components/project-section.jsx
+++ src/components/project-section.jsx
@@ -6,30 +6,46 @@ function Projects(){
             <div id="proj-cont">
             <div className="project-parentDiv">
                 <div className="project-1">
-                    <div>
-                        {/* <img src="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8M
-                        HxzZWFyY2h8MTB8fHByb2plY3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="project"/> */}
-                    <img src="https://www.w3schools.com/images/w3schools_green.jpg" alt="W3Schools.com"/>
+                    <div className="project-img">
+                        {/* <img></img> */}
                     </div>
                     <div className="project-description">
-                    <p>Lorem Ipsum jekajade lo si ode. Iwo na mo wipe mo feran re.
-                        Inu mi ma tutu ti o ba sowipe o nbo wa si odo mi.
-                    </p>
+                        <p>Lorem Ipsum jekajade lo si ode. Iwo na mo wipe mo feran re.
+                            Inu mi ma tutu ti o ba sowipe o nbo wa si odo mi.
+                        </p>
                     </div>
                 </div>
-
-                <div className="project-2">
-
+                <div className="project-1">
+                    <div className="project-img">
+                        {/* <img></img> */}
+                    </div>
+                    <div className="project-description">
+                        <p>Lorem Ipsum jekajade lo si ode. Iwo na mo wipe mo feran re.
+                            Inu mi ma tutu ti o ba sowipe o nbo wa si odo mi.
+                        </p>
+                    </div>
                 </div>
-
-                <div className="project-3">
+                <div className="project-1">
+                    <div className="project-img">
+                        {/* <img></img> */}
+                    </div>
+                    <div className="project-description">
+                        <p>Lorem Ipsum jekajade lo si ode. Iwo na mo wipe mo feran re.
+                            Inu mi ma tutu ti o ba sowipe o nbo wa si odo mi.
+                        </p>
+                    </div>
+                </div>
+                <div className="project-1">
+                    <div className="project-img">
+                        {/* <img></img> */}
+                    </div>
+                    <div className="project-description">
+                        <p>Lorem Ipsum jekajade lo si ode. Iwo na mo wipe mo feran re.
+                            Inu mi ma tutu ti o ba sowipe o nbo wa si odo mi.
+                        </p>
+                    </div>
+                </div>
                 
-                </div>
-
-                <div className="project-4">
-
-                </div>
-
             </div>    
             </div>
 </div>
