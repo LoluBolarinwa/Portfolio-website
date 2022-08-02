f9684b7707bde55cb02618a9cab7f0a781f9ceb6
LoluBolarinwa <lolubolarinwa@gmail.com>
2022-08-02 12:25:52 +0100

    project section structure

--- src/components/project-section.jsx
+++ src/components/project-section.jsx
@@ -6,14 +6,14 @@ function Projects(){
             <div id="proj-cont">
             <div className="project-parentDiv">
                 <div className="project-1">
-                <div>
-                    <img src="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2plY3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="project" width="200" height="200"/>
-                
-                ""    
-                </div>
+                    <div>
+                        {/* <img src="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8M
+                        HxzZWFyY2h8MTB8fHByb2plY3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="project"/> */}
+                    <img src="https://www.w3schools.com/images/w3schools_green.jpg" alt="W3Schools.com"/>
+                    </div>
                     <div className="project-description">
-                    <p >Lorem Ipsum jekajade lo si ode. Iwo na mo wipe mo feran re.
-                    Inu mi ma tutu ti o ba sowipe o nbo wa si odo mi.
+                    <p>Lorem Ipsum jekajade lo si ode. Iwo na mo wipe mo feran re.
+                        Inu mi ma tutu ti o ba sowipe o nbo wa si odo mi.
                     </p>
                     </div>
                 </div>
